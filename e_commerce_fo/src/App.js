715752c46e8acf45dd397bb18b7155249bf4c1bb
sabri-jammoussi <sabrijm123@gmail.com>
2025-05-03 15:59:16 +0100

    change the router

--- e_commerce_fo/src/App.js
+++ e_commerce_fo/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import ProductList from "./pages/ProductList"
+import ProductView from "./components/ProductView";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+
+// Wrapper to access `useLocation` outside Router
+function AppContent() {
+  const location = useLocation();
+  const hideNavbarPaths = ["/login"]; // Add more paths if needed
+
+  return (
+    <>
+      {!hideNavbarPaths.includes(location.pathname) && <Navbar />}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<ProductList />} />
+        <Route path="/productView" element={<ProductView />} />
+
+        {/* Add other routes here */}
+      </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppContent />
+    </Router>
   );
 }
 
